e02781cf78b278ac705605c8c08f42bb0bf45a4d
tomislav-ficko <ficko.tomislav@gmail.com>
2020-09-14 23:04:59 +0200

    Migrate app to AndroidX

--- app/src/main/java/com/ficko/flashchat/MainChatActivity.java
+++ app/src/main/java/com/ficko/flashchat/MainChatActivity.java
@@ -1,7 +1,7 @@
 package com.ficko.flashchat;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
--- app/src/main/java/com/ficko/flashchat/RegisterActivity.java
+++ app/src/main/java/com/ficko/flashchat/RegisterActivity.java
@@ -1,8 +1,9 @@
 package com.ficko.flashchat;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
