2accd29bb7ea28eb392a3c1176e386b700074870
tomislav-ficko <ficko.tomislav@gmail.com>
2020-09-18 17:48:13 +0200

    Add ListAdapter, the logic for retrieving data from Firebase and code for populating ListView items

---
+++ app/src/main/java/com/ficko/flashchat/ChatListAdapter.java
@@ -0,0 +1,117 @@
+package com.ficko.flashchat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+public class ChatListAdapter extends BaseAdapter {
+
+    private Activity mActivity;
+    private DatabaseReference mDatabaseReference;
+    private String mDisplayName;
+    private ArrayList<DataSnapshot> mSnapshotList;
+
+    private ChildEventListener mListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            mSnapshotList.add(snapshot);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    };
+
+    public ChatListAdapter(Activity activity, DatabaseReference databaseReference, String displayName) {
+        mActivity = activity;
+        mDisplayName = displayName;
+
+        mDatabaseReference = databaseReference.child("messages");
+        mDatabaseReference.addChildEventListener(mListener);
+
+        mSnapshotList = new ArrayList<>();
+    }
+
+    // Inner class which holds all the views within a single chat row
+    static class ViewHolder {
+        TextView authorName;
+        TextView body;
+        LinearLayout.LayoutParams params;
+    }
+
+    @Override
+    public int getCount() {
+        return mSnapshotList.size();
+    }
+
+    @Override
+    public InstantMessage getItem(int position) {
+        DataSnapshot snapshot = mSnapshotList.get(position);
+        return snapshot.getValue(InstantMessage.class);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+
+            // Create new row from the layout file
+            LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.chat_msg_row, parent, false);
+
+            final ViewHolder holder = new ViewHolder();
+
+            holder.authorName = convertView.findViewById(R.id.author);
+            holder.body = convertView.findViewById(R.id.message);
+            holder.params = (LinearLayout.LayoutParams) holder.authorName.getLayoutParams();
+
+            convertView.setTag(holder);
+        }
+
+        final ViewHolder holder = (ViewHolder) convertView.getTag();
+        final InstantMessage message = getItem(position);
+
+        String author = message.getAuthor();
+        String msg = message.getMessage();
+        holder.authorName.setText(author);
+        holder.body.setText(msg);
+
+        return convertView;
+    }
+}
