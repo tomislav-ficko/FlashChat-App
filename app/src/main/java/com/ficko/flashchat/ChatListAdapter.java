78a87be39e7ff0b348e37fd4080d086c52925eff
tomislav-ficko <ficko.tomislav@gmail.com>
2020-09-18 18:06:20 +0200

    Change appearance of messages based on the sender

--- app/src/main/java/com/ficko/flashchat/ChatListAdapter.java
+++ app/src/main/java/com/ficko/flashchat/ChatListAdapter.java
@@ -2,6 +2,8 @@ package com.ficko.flashchat;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -107,12 +109,30 @@ public class ChatListAdapter extends BaseAdapter {
         final ViewHolder holder = (ViewHolder) convertView.getTag();
         final InstantMessage message = getItem(position);
 
+        boolean isMe = message.getAuthor().equals(mDisplayName);
+        setChatRowAppearance(isMe, holder);
+
         String author = message.getAuthor();
         String msg = message.getMessage();
         holder.authorName.setText(author);
         holder.body.setText(msg);
 
         return convertView;
+    }
+
+    private void setChatRowAppearance(boolean isItMe, ViewHolder holder) {
+        if (isItMe) {
+            holder.params.gravity = Gravity.END;
+            holder.authorName.setTextColor(Color.GREEN);
+            holder.body.setBackgroundResource(R.drawable.bubble2);
+        } else {
+            holder.params.gravity = Gravity.START;
+            holder.authorName.setTextColor(Color.BLUE);
+            holder.body.setBackgroundResource(R.drawable.bubble1);
+        }
+
+        holder.authorName.setLayoutParams(holder.params);
+        holder.body.setLayoutParams(holder.params);
     }
 
     public void cleanup() {
